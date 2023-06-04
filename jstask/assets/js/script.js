c664c1d8ef38b850c294743fdb8b42002487ac59
amirovstar <furkanza@code.edu.az>
2023-06-05 01:16:33 +0400

    done

---
+++ jstask/assets/js/script.js
@@ -0,0 +1,41 @@
+'use strict'
+
+//#region Task 1
+
+// table();
+// function table() {
+//     let table = document.getElementById("tableid");
+//     let result = "";
+  
+//     for (let i = 0; i < 8; i++) {
+//       let row = "<tr>";
+  
+//       for (let j = 0; j < 8; j++) {
+//         let td = `<td class="${(i + j) % 2 === 0 ? 'text-bg-dark' : ''}" style="width: 60px; height: 60px;"></td>`;
+//         row += td;
+//       }
+  
+//       row += "</tr>";
+//       result += row;
+//     }
+  
+//     table.innerHTML = result;
+//   }
+  //#endregion
+ 
+ //#region task 2
+//   function validation() {
+//     let username = "admin@gmail.com";
+//     let password = "12345";
+//     let userid = document.getElementById("username").value;
+//     let passid = document.getElementById("password").value;
+  
+//     if (userid === username && passid === password) {
+//       alert("Your log in process was succesful!");
+//     } else {
+//       alert("Your credentials are incorrect!");
+//     }
+//   }
+  //#endregion
+  
+  
